70924448af28c3c333d8d686b0a882a13ddd0313
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-03-30 10:29:39 -0300

    instala boostrap e react-bootstrap

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+    <button className = "btn btn-primary">Teste</button>
+    <Button variant="success">Teste</Button>
+    </div>
+  );
+}
+
+export default App;
