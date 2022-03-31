c00c35e7ac814e93f3e5657ef45d9ce7712e43b7
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-03-31 13:52:41 -0300

    implementa o header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,42 @@
-import { Button } from "react-bootstrap";
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import Logo from './assets/img/logo-jn.png'
+
+
+function Header() {
+  return (
+    <header>
+      <Navbar expand='md' className='main-navbar'>
+      <Container>
+        <Navbar.Brand href='/'>
+          <img src={Logo} alt='JN Mecânica Automotiva'/> 
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbar-header' />
+        <Navbar.Collapse id='navbar-header' >
+        <Nav className='ms-auto'>
+        <Nav.Link href='/'>Home</Nav.Link>
+        <Nav.Link href='/serviços'>Serviços</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+      </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+
+function HomeView() {
+  return (
+    <>
+    <Header />
+    <p>Conteúdo</p>
+    <p>Footer</p>
+    </>
+  )
+
+}
 
 function App() {
-  return (
-    <div>
-    <button className = "btn btn-primary">Teste</button>
-    <Button variant="success">Teste</Button>
-    </div>
-  );
+  return <HomeView />
 }
 
 export default App;
