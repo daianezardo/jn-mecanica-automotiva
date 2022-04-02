c007b13b8c81dd57f12be3e8d0199b882a698306
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-04-02 12:05:18 -0300

    altera links para React Router Links

--- front-end/src/components/Layout/header.js
+++ front-end/src/components/Layout/header.js
@@ -1,4 +1,5 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Logo from '../../assets/img/logo-jn.png'
 
 export function Header() {
@@ -6,14 +7,14 @@ export function Header() {
       <header>
         <Navbar expand='md' className='main-navbar'>
         <Container>
-          <Navbar.Brand href='/'>
+          <Navbar.Brand as={Link} to='/'>
             <img src={Logo} alt='JN Mecânica Automotiva' width={115} height={75} /> 
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar-header' />
           <Navbar.Collapse id='navbar-header' >
           <Nav className='ms-auto'>
-          <Nav.Link href='/'>Home</Nav.Link>
-          <Nav.Link href='/serviços'>Serviços</Nav.Link>
+          <Nav.Link as={Link} to='/'>Home</Nav.Link>
+          <Nav.Link as={Link} to='/serviços'>Serviços</Nav.Link>
           </Nav>
           </Navbar.Collapse>
         </Container>
--- front-end/src/views/home/index.js
+++ front-end/src/views/home/index.js
@@ -1,6 +1,7 @@
 import { Button, Container } from "react-bootstrap";
 import { Layout } from "../../components/Layout";
 import Car from "../../assets/img/car.jpg"
+import { Link } from "react-router-dom";
 
 
 export function HomeView() {
@@ -12,7 +13,7 @@ export function HomeView() {
      <h1>Bem vindo(a) à JN!</h1>
      <p>Conheça nossos serviços</p>
      <p>Faça seu agendamento agora mesmo.</p>
-     <Button className='text-uppercase btn-services'>Serviços</Button>
+     <Button as={Link} to='/serviços' className='text-uppercase btn-services'>Serviços</Button>
           </div>
           <div>
      <img src={Car} alt='JN Mecânica Automotiva' width={360} height={225} className='img-fluid'/>
