0842ba404930d6b2b8bcfa2f799ee7fad5ff2152
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-04-05 17:57:26 -0300

    excluí arquivo netlify

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/css/styles.css'
-import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.render(
   <React.StrictMode>
