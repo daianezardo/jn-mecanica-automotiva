bb573ac8bf25758d2c9abcee7d5c3ca8e42f25d3
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-05-22 23:41:32 -0300

    Instala o redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,14 +6,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/css/styles.css'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './store/store'
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <ReduxProvider store={store}>
     <BrowserRouter>
       <App />
       <ToastContainer/>
     </BrowserRouter>
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ front-end/src/store/store.js
@@ -0,0 +1,6 @@
+import { createStore } from 'redux'
+
+const reducer = (state, action) => {
+
+}
+export const store = createStore(reducer, window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_())
