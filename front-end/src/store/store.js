6bac954be4354bb7f2b1fd2c6205ebea3e4bfceb
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-05-30 16:34:13 -0300

    Move reducer para arquivo próprio

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -1,20 +1,8 @@
 import { createStore } from 'redux'
-import { getStorageItem } from '../service/storage.service'
+import { userReducer } from './User/User.reducer'
 
-const user = JSON.parse(getStorageItem('user'))
 
-const reducer = (state, action) => {
-    console.log(state, action)
-    if (action.type === '@@INIT') {
-        return user || null
-    }
-    if (action.type === 'USER_LOGIN') {
-        return action.payload
-    }
-    if (action.type === 'USER_LOGOUT') {
-        return null
-    }
-
-    return state
-}
-export const store = createStore(reducer, window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_())
+export const store = createStore(
+    userReducer, 
+    window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+    )
