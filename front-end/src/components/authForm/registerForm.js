af1332f860a1f8b256676a38e50d15fe913b741a
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2022-06-09 23:29:07 -0300

    Faz cadastro funcionar

--- front-end/src/components/authForm/registerForm.js
+++ front-end/src/components/authForm/registerForm.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom";
+import { createUser } from "../../service/users.service";
+import { userLogin } from "../../store/User/User.action";
+import { toast } from "react-toastify";
+
 
 export function RegisterForm () {
     const [formData, setFormData] = useState({
@@ -13,8 +19,20 @@ export function RegisterForm () {
             [event.target.name]: event.target.value 
         })
     }
-    const handleSubmit = (event) => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const handleSubmit = async (event) => {
         event.preventDefault()
+        try {
+            const userData = await createUser(formData)
+            dispatch(userLogin(userData))
+            navigate('/portal')
+        } catch (error) {
+            const message = error.message === 'Email already exists'
+            ? 'Este e-mail já está em uso.'
+            : 'Falha ao fazer cadastro. Tente novamente.'
+            toast.error(message)     
+        }
     }
     return (
         <Form onSubmit={handleSubmit}>
@@ -29,7 +47,7 @@ export function RegisterForm () {
             </Form.Group>
             <Form.Group className="mb-3" controlId="register-password">
                 <Form.Label className="m-0">Senha</Form.Label>
-                <Form.Control type="password" placeholder="Senha de acesso" value={formData.password} onChange={handleChange} name="password" required/>
+                <Form.Control type="password" placeholder="Senha de acesso" value={formData.password} onChange={handleChange} name="password" required minLength={4}/>
             </Form.Group>
             <Button type="submit" className="btn-services">Criar conta</Button>
         </Form>
--- front-end/src/service/users.service.js
+++ front-end/src/service/users.service.js
@@ -16,14 +16,41 @@ export const login = async (credentialsData) => {
         : 'Response not ok.'
         throw new Error(message)
     }
+    return processAuthResponse(data)
+}
+
+export const logout = () => {
+    removeStorageItem('user')
+}
+
+export const createUser = async (userData) => {
+    const body = JSON.stringify({
+        ...userData,
+        type: 2
+    })
+    const response = await fetch(`${apiUrl}/signup`, {
+        method: 'POST',
+        body,
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    const data = await response.json()
+    if (!response.ok) {
+        const message = typeof data === 'string'
+        ? data
+        : 'Response not ok.'
+        throw new Error(message)
+    }
+
+    return processAuthResponse(data)
+}
+
+const processAuthResponse = (data) => {
     const userData = {
         accessToken: data.accessToken,
         ...data.user
     }
     setStorageItem('user',  JSON.stringify(userData))
     return userData
-}
-
-export const logout = () => {
-    removeStorageItem('user')
 }
